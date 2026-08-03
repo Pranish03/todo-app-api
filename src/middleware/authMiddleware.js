bf2700fea7f97e6c80761786c9f54e9836d86c0a
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-08-03 13:46:23 +0545

    Finish auth middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,1 +1,36 @@
-export default authMiddleware;
+import jwt from "jsonwebtoken";
+import { prisma } from "../config/db.js";
+
+export async function protect(request, response, next) {
+  try {
+    const token = request.cookies["auth-token"];
+
+    if (!token) {
+      response.status(401).json({ success: false, message: "Not authorized" });
+      return;
+    }
+
+    const JWT_SECRET = process.env.JWT_SECRET;
+
+    if (!JWT_SECRET) {
+      throw new Error("JWT_SECRET is not defined in environment variables");
+    }
+
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    const user = await prisma.user.findUnique({
+      where: { id: decoded.userId },
+    });
+
+    if (!user) {
+      response.status(401).json({ success: false, message: "Not authorized" });
+      return;
+    }
+
+    request.user = user;
+
+    next();
+  } catch (error) {
+    response.status(401).json({ success: false, message: "Not authorized" });
+  }
+}
