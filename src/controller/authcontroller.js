a756e9d6c12d3d1663159b57bee1b937491700fa
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-08-03 12:10:51 +0545

    Setup controller file

---
+++ src/controller/authcontroller.js
@@ -0,0 +1,43 @@
+export const register = () => {
+  try {
+    // Code below
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+
+export const login = () => {
+  try {
+    // Code below
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+
+export const logout = () => {
+  try {
+    // Code below
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+
+export const me = () => {
+  try {
+    // Code below
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
