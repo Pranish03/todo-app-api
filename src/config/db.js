86fb5c9d97b2acaa3a7275e369cb7d31b2502231
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-08-03 12:16:14 +0545

    configure database

--- src/config/db.js
+++ src/config/db.js
@@ -1,3 +1,26 @@
+import { PrismaClient } from "../src/generated/prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL,
+});
 
+const prisma = new PrismaClient({
+  adapter,
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "error", "warn"]
+      : ["error"],
+});
+
+const connectDatabase = async () => {
+  try {
+    await prisma.$connect();
+    console.log("Database connected successfully!");
+  } catch (error) {
+    console.error(`Database connection error!, Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export { prisma, connectDatabase };
